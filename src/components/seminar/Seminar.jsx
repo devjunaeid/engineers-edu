32cfec65fc21a7128b01afee3aee95ba137d3a69
devjunaeid <junaeid.md48@gmail.com>
2024-01-29 18:30:52 +0600

    Added: ReactQuery and Server action

--- src/components/seminar/Seminar.jsx
+++ src/components/seminar/Seminar.jsx
@@ -1,20 +1,15 @@
 "use client";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getSeminars } from "@/server/actions/actions";
 
 function Seminar() {
-  const [data, setData] = useState([]);
-
-  const getData = () => {
-    return fetch("http://localhost:3000/api/seminars")
-      .then((res) => res.json())
-      .then((d) => setData(d));
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
+  const { data, isLoading } = useQuery({
+    queryKey: ["get-seminars"],
+    queryFn: () => getSeminars(),
+  });
+  
   return (
     <div>
       <div className="overflow-x-auto">
@@ -31,18 +26,20 @@ function Seminar() {
             </tr>
           </thead>
           <tbody className="text-sm md:text-lg">
-            {
-                data?.map((item, index) => (
-                    <tr key={item.id} className="hover:bg-amber-100">
-                        <th>{index + 1}</th>
-                        <td>{item.title}</td>
-                        <td>{item.platfrom}</td>
-                        <td>{item.date}</td>
-                        <td>{item.time}</td>
-                        <td><Link className="btn" href={item.link}>Join</Link></td>
-                    </tr>
-                ))
-            }
+          {isLoading ? (<tr>Loading...</tr>) : (data?.map((item, index) => (
+                <tr key={item.id} className="hover:bg-amber-100">
+                  <th>{index + 1}</th>
+                  <td>{item.title}</td>
+                  <td>{item.platfrom}</td>
+                  <td>{item.date}</td>
+                  <td>{item.time}</td>
+                  <td>
+                    <Link className="btn" href={item.link}>
+                      Join
+                    </Link>
+                  </td>
+                </tr>
+              )))}
           </tbody>
         </table>
       </div>
